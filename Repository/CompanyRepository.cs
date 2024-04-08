2c42ccf27087fee3d0c614a9525d2f69b2528467
necatihan <han.necati@gmail.com>
2024-04-08 02:19:21 +0200

    Add first controller

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -8,4 +8,11 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    {
+        return FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
+    }
 }
