53b391b811134d644ac08e99646b10fd4fa8d881
necatihan <han.necati@gmail.com>
2024-04-12 10:47:28 +0200

    Add configured responses

---
+++ Entities/Responses/ApiBaseResponse.cs
@@ -0,0 +1,7 @@
+namespace Entities.Responses;
+
+public abstract class ApiBaseResponse
+{
+    public bool Success { get; set; }
+    protected ApiBaseResponse(bool success) => Success = success;
+}
